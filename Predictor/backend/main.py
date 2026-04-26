0f07a6b7ea56271b21842acb903741297c5235e2
nishrajpal05 <47219617+nishrajpal05@users.noreply.github.com>
2026-04-26 20:32:27 +0530

    Update main.py

--- Predictor/backend/main.py
+++ Predictor/backend/main.py
@@ -258,10 +258,10 @@ def get_blend():
 
 
 
-FRONTEND_DIR = os.path.join(os.path.dirname(__file__), "..", "frontend")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 
 app.mount("/static", StaticFiles(directory=FRONTEND_DIR), name="static")
-
 
 @app.get("/")
 def serve_frontend():
